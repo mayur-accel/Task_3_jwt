8fb29e76fd9545f432eef829947485d0ea7626c7
mayur-accel <mayur.p@accelfintech.com>
2024-05-20 19:01:45 +0530

    feat: passsort js related demo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
+import session from "express-session";
+import passport from "passport";
 import { connectDatabase } from "./db/dbConnection";
 import { errorHandler } from "./middleware/errorHandler.middleware";
+import "./middleware/googleAuth.middleware";
 import RootRouter from "./routes/rootRouter.routes";
 
 dotenv.config();
@@ -10,8 +13,38 @@ const app = express();
 connectDatabase();
 
 app.use(express.json());
+app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: "key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/api/v1", RootRouter);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/auth/google/success",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+app.get("/auth/logout", async (req: Request, res: Response) => {
+  req.logout((err) => {
+    if (err) console.log(err);
+  });
+});
 
 // Register the error handler middleware
 app.use(errorHandler);
---
+++ src/middleware/googleAuth.middleware.ts
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import { Request } from "express";
+import passport from "passport";
+import {
+  Strategy as GoogleStrategy,
+  VerifyCallback,
+} from "passport-google-oauth2";
+import User from "../models/user.models";
+dotenv.config();
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    async (
+      request: Request,
+      accessToken: string,
+      refreshToken: string,
+      profile: any,
+      done: VerifyCallback
+    ) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            email: profile.emails[0].value, // Assuming the email is in the first position
+            firstName: profile.given_name,
+            lastName: profile.family_name,
+            password: profile.emails[0].value,
+          });
+
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user: any, done) => {
+  done(null, user);
+});
