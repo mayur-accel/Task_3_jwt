1ad835248d16004ad0585fe46e852f255e9a9e14
mayur-accel <mayur.p@accelfintech.com>
2024-05-27 11:40:33 +0530

    feat: eslint and prettier install

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -6,10 +6,15 @@ const dbURI = config.get("dbURI");
 
 export const connectDatabase = async () => {
   try {
+    mongoose.connection.on("connected", () => {
+      console.log(
+        `${colors.FgGreen}Database connected successfully${colors.Reset}`
+      );
+    });
+    mongoose.connection.on("error", (err) => {
+      console.log(`${colors.FgRed}Database connection fail ${colors.Reset}`);
+    });
     await mongoose.connect(dbURI);
-    console.log(
-      `${colors.FgGreen}Database connected successfully${colors.Reset}`
-    );
   } catch (error: any) {
     console.log(
       `${colors.FgRed}Database connection error: ${error.message}${colors.Reset}`
